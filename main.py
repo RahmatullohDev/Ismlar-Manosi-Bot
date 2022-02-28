3959460565a46230be36534a05711b8bff9ad8ef
RahmatullohDev <100528967+RahmatullohDev@users.noreply.github.com>
2022-02-28 18:19:40 +0500

    Add files via upload
    Ushbu kod ism.uz saytidan ismlar manosini topib berish uchun mo'ljallangan.Token qo'ysangiz ishlaydi.

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot, requests
+from bs4 import BeautifulSoup
+
+bot = telebot.TeleBot("BOT_TOKEN") 
+tegtext = "👉 @Unknown_26_04"
+
+@bot.message_handler(commands=['start','START','Start'])
+def start(message):
+    bot.send_message(message.chat.id,f"Assalomu Alaykum <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>!\n\nUshbu bot orqali ismlarning ma'nosini bilib olishingiz mumkin!\n\nBuning uchun menga ism jo'nating va men sizga uni ma'nosini jo'nataman!",
+    parse_mode='html')
+
+@bot.message_handler(content_types=['text'])
+def ism(message):
+        try:
+            url = requests.get(f"https://ismlar.com/search/{message.text}")
+            soup = BeautifulSoup(url.text,'lxml')
+            mano = soup.find('p',class_='text-size-5').text
+            bot.reply_to(message,f"<b>🔎Ism:</b> {message.text}\n\n<b>📑 Ma'nosi:</b> <code>{mano}</code>\n\n{tegtext}",
+            parse_mode='html')
+        except:
+            bot.reply_to(message,f"{message.text} ism mano'si topilmadi.Ismni to'g'ri yozganingizga ishonch xosil qiling!")
+
+bot.polling()
